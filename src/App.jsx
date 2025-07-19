4de00f12d938c6517cc7bd6d0b37da7ec26b1ea0
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-07-19 21:00:03 +0600

    Footer Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Projects />
       <Testimonials />
       <Contact />
+      <Footer />
 
     </div>
   )
