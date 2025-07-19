2a11f4471acf8c0146966f980c582ebb53e0f438
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-07-19 21:20:53 +0600

    Animation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { motion } from "motion/react"
 
 const Header = () => {
   return (
@@ -8,15 +9,20 @@ const Header = () => {
         backgroundImage: "url('/header_img.png')"
       }}
       id='Header'>
-<Navbar />
-<div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
-  <h2 className='text-5xl sm:6xl md:text-[82px] inline-block mx-w-3xl font-semibold pt-20'>Explore Homes that fits your dream</h2>
-  <div className='space-x-6 mt-16'>
-    <a href="#Projects" className='border border-white px-8 py-3 rounded-lg'>Projects</a>
-    <a href="#Contact" className='bg-blue-500 border-white px-8 py-3 rounded-lg'>Contact Us</a>
-  </div>
-</div>
+      <Navbar />
+      <motion.div initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+        <h2 className='text-5xl sm:6xl md:text-[82px] inline-block mx-w-3xl font-semibold pt-20'>Explore Homes that fits your dream</h2>
+        <div className='space-x-6 mt-16'>
+          <a href="#Projects" className='border border-white px-8 py-3 rounded-lg'>Projects</a>
+          <a href="#Contact" className='bg-blue-500 border-white px-8 py-3 rounded-lg'>Contact Us</a>
+        </div>
+      </motion.div>
     </div>
+
 
   )
 }
